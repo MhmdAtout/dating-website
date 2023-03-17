7542579e05b28f12cfe5b609e490445dd64081e6
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-17 21:52:15 +0200

    adding database tables"

---
+++ backend/database/migrations/2023_03_17_190125_users_info.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('id')->on('users');
+            $table->string('bio');
+            $table->string('prodile_pic');
+            $table->string('optional_pic1');
+            $table->string('optional_pic2');
+            $table->string('optional_pic3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
