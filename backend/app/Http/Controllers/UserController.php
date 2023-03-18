67bed42a6dd240cff45b59ddad1f577ecf849395
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-18 08:10:41 +0200

    backend: get all users function

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    function getUsers(){
+        $users = User::all();
+
+        return response()->json([
+            "users" => $users
+        ]);
+    }
 }
