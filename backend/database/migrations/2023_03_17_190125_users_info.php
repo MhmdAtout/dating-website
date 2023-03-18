7713c156c6817f124421446e33ebdaa50de74b59
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-18 18:16:01 +0200

    backend: editing table structure

--- backend/database/migrations/2023_03_17_190125_users_info.php
+++ backend/database/migrations/2023_03_17_190125_users_info.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users_info', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained('id')->on('users');
-            $table->string('bio');
             $table->string('prodile_pic');
             $table->string('optional_pic1');
             $table->string('optional_pic2');
