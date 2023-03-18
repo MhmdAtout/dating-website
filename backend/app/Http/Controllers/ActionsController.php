f515eaff567ee81d706434a1035695ca317276a7
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-18 12:52:41 +0200

    backend: adding follow api

--- backend/app/Http/Controllers/ActionsController.php
+++ backend/app/Http/Controllers/ActionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use App\Models\Follower;
 
 class ActionsController extends Controller
 {
@@ -31,5 +32,30 @@ class ActionsController extends Controller
         return response()->json([
             "response" => $message
         ]);
+    }
+
+    function follow(Request $request){
+        $follow = Follower::where("follower_id", $request->follower_id)
+                            ->where("followed_id", $request->followed_id)
+                            ->get();
+        if($follow->count()!=0){
+            $follow = Follower::where("follower_id", $request->follower_id)
+                            ->where("followed_id", $request->followed_id)
+                            ->delete();
+
+            return response()->json([
+                "status" => "unfollowed"
+            ]);
+        }else{
+            $follow = Follower::create([
+                "follower_id" => $request->follower_id,
+                "followed_id" => $request->followed_id,
+            ]);
+    
+            return response()->json([
+                "message" => $follow,
+                "status" => "followed"
+            ]);
+        }
     }
 }
