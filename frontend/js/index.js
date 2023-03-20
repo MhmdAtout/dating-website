c3f2530e18577802cc07af0c3a7494276fb22a5a
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-20 08:03:55 +0200

    frontend: fetching signin API

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,16 +1,15 @@
-// const  = document.getElementById("");
 const signup_page = document.getElementById("signup_page");
 const signin_page = document.getElementById("signin_page");
 const to_signin = document.getElementById("to_signin");
 const to_signup = document.getElementById("to_signup");
 
+// Navigiating between signup and signin
 to_signin.addEventListener("click", () => {
   signin_page.classList.remove("hide");
   signin_page.classList.add("flex");
   signup_page.classList.remove("flex");
   signup_page.classList.add("hide");
 });
-
 to_signup.addEventListener("click", () => {
   signup_page.classList.remove("hide");
   signup_page.classList.add("flex");
@@ -18,7 +17,7 @@ to_signup.addEventListener("click", () => {
   signin_page.classList.add("hide");
 });
 
-const register = document.getElementById("register");
+const register_btn = document.getElementById("register_btn");
 const name_signup_input = document.getElementById("name_signup_input");
 const email_signup_input = document.getElementById("email_signup_input");
 const password_signup = document.getElementById("password_signup_input");
@@ -27,7 +26,8 @@ const gender_signup_input = document.getElementById("gender_signup_input");
 const location_signup_input = document.getElementById("location_signup_input");
 const signup_error = document.getElementById("signup_error");
 
-register.addEventListener("click", () => {
+// Fetching signup API
+register_btn.addEventListener("click", () => {
   let register_data = new FormData();
   register_data.append("name", name_signup_input.value);
   register_data.append("email", email_signup_input.value);
@@ -45,4 +45,29 @@ register.addEventListener("click", () => {
     localStorage.setItem("id", res.data.user.id);
     window.location.href = "./pages/landing.html";
   });
+});
+
+const email_signin_input = document.getElementById("email_signin_input");
+const password_signin_input = document.getElementById("password_signin_input");
+const login_btn = document.getElementById("login_btn");
+const signin_error = document.getElementById("signin_error");
+
+login_btn.addEventListener("click", () => {
+  let login_data = new FormData();
+  login_data.append("email", email_signin_input.value);
+  login_data.append("password", password_signin_input.value);
+
+  axios({
+    method: "post",
+    url: "http://localhost:8002/api/auth/login",
+    data: login_data,
+  })
+    .then((res) => {
+      localStorage.setItem("id", res.data.user.id);
+      localStorage.setItem("access_token", res.data.authorisation.token);
+      window.location.href = "./pages/landing.html";
+    })
+    .catch((error) => {
+      signin_error.innerHTML = error.response.data.message;
+    });
 });
