7fc1363cc82e0e2bc5339dac5188d510e4707e97
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-20 10:13:07 +0200

    frontend: fetching  users from opposite gender

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -8,6 +8,9 @@ const notifications_page = document.getElementById("notifications_page");
 const message_page = document.getElementById("message_page");
 const profile_page = document.getElementById("profile_page");
 
+const user_id = localStorage.getItem("id");
+const baseURL = "http://localhost:8002/api";
+
 home_nav_btn.addEventListener("click", (e) => {
   e.preventDefault();
   main_page.classList.remove("hide");
@@ -28,4 +31,47 @@ notification_nav_btn.addEventListener("click", (e) => {
   message_page.classList.add("hide");
   notifications_page.classList.remove("hide");
   notifications_page.classList.add("flex");
+});
+
+const users_list = document.getElementById("users_list");
+axios({
+  method: "get",
+  url: `${baseURL}/user/allUsers/${user_id}`,
+}).then((res) => {
+  let users = res.data.users;
+  users.forEach((user) => {
+    users_list.innerHTML += `
+    <div class="user-card">
+              <div class="user-main-info flex column jc-center ai-center">
+                <div class="user-image">
+                  <img class="user-image" src="" alt="" />
+                </div>
+                <div class="user-name flex jc-center ai-center">
+                  <h3>${user.name}</h3>
+                </div>
+              </div>
+              <div class="user-other-info flex column jc-center ai-center">
+                <div class="user-age">
+                  <p>${user.age}</p>
+                </div>
+                <div class="user-location">
+                  <p>${user.location}</p>
+                </div>
+              </div>
+              <div class="card-actions flex column jc-se ai-center">
+                <div class="message flex">
+                  <button class="message_user_btn" value="${user.id}">Message</button>
+                </div>
+                <div class="flex ai-center jc-center">
+                  <div class="follow">
+                    <button value="${user.id}">Follow</button>
+                  </div>
+                  <div class="block">
+                    <button value="${user.id}">Block</button>
+                  </div>
+                </div>
+              </div>
+    </div>
+    `;
+  });
 });
