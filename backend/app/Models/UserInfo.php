a8a5997d83428f42771e4285ef5bec2c82047b70
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-21 22:12:53 +0200

    backend: adding a model

---
+++ backend/app/Models/UserInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserInfo extends Model
+{
+    use HasFactory;
+
+    protected $table = "users_info";
+
+    protected $fillable = [
+        "user_id",
+        "profile_pic",
+        "optional_pic1",
+        "optional_pic2", 
+        "optional_pic3"
+    ];
+}
